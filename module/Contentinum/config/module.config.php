72a20b246f490b9ebee419ba7615ad92d4e067eb
mikel1961 <michael.jochum@jochum-mediaservices.de>
2013-08-13 10:32:23 +0200

    Add translate support
    Change-Id: I636711faa5084585b5e9540e1ae003f1ebb99dc4

--- module/Contentinum/config/module.config.php
+++ module/Contentinum/config/module.config.php
@@ -13,8 +13,28 @@ return array (
 								) 
 						) 
 				) 
-		)
-		,
+		),
+		'service_manager' => array (
+				
+				'factories' => array (
+						'Contentinum\Configure' => 'Contentinum\Service\ConfigServiceFactory',
+						'Contentinum\Logs' => 'Contentinum\Service\LogServiceFactory',
+						'Contentinum\Logs\Applog' => 'Contentinum\Service\ApplogServiceFactory',
+				),
+				'aliases' => array (
+						'translator' => 'MvcTranslator' 
+				),		
+		),
+		'translator' => array (
+				'locale' => 'de_DE',
+				'translation_file_patterns' => array (
+						array (
+								'type' => 'gettext',
+								'base_dir' => __DIR__ . '/../language',
+								'pattern' => '%s.mo' 
+						) 
+				) 
+		),
 		'controllers' => array (
 				'invokables' => array (
 						'Contentinum\Controller\Index' => 'Contentinum\Controller\IndexController' 
@@ -34,6 +54,81 @@ return array (
 				),
 				'template_path_stack' => array (
 						__DIR__ . '/../view' 
+				) 
+		),
+		'contentinum_config' => array (
+				'log_configure' => array (
+						'log_priority' => 6,
+						'log_writer' => array(
+						'application' => CON_ROOT_PATH . '/data/logs/application.log',
+						'error' =>  CON_ROOT_PATH . '/data/logs/errors.application.log',
+						),
+						'log_filter' => array(
+							'application' => array('priority' => array(  'priority' => Zend\Log\Logger::WARN, 'operator' => '>=')),
+							'error' => array('priority' => array( 'priority' => Zend\Log\Logger::ERR, 'operator' => '<='))
+						),
+				),
+				'contentinum_acl' => array (
+						'acl_default_role' => 'guest',
+						'acl_settings' => array (
+								'roles' => array (
+										'guest',
+										'member',
+										'author',
+										'publisher',
+										'manager',
+										'admin',
+										'root' 
+								),
+								'parent' => array (
+										'member' => 'guest',
+										'author' => 'member',
+										'publisher' => 'author',
+										'manager' => 'publisher',
+										'admin' => 'manager',
+										'root' => 'admin' 
+								),
+								
+								'resources' => array (
+										'index',
+										'error',
+										'medias',
+										'memberresource',
+										'authorresource',
+										'publisherresource',
+										'managerresource',
+										'adminresource',
+										'rootresource' 
+								),
+								
+								'rules' => array (
+										'allow' => array (
+												'guest' => array (
+														'index' => 'all',
+														'error' => 'all',
+														'medias' => 'all' 
+												),
+												'member' => array (
+														'memberresource' => 'all' 
+												),
+												'author' => array (
+														'authorresource' => 'all' 
+												),
+												'publisher' => array (
+														'publisherresource' => 'all' 
+												),
+												'manager' => array (
+														'managerresource' => 'all' 
+												),
+												'admin' => array (
+														'adminresource' => 'all' 
+												),
+												'root' => array (
+														'rootresource' => 'all' 
+												) 
+										) 
+								) 
+						) 
 				) 
 		) 
 );
