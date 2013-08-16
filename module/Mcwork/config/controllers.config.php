d4029023c6d0268209d6cd6fff4ddc81ac0ab8f8
mikel1961 <michael.jochum@jochum-mediaservices.de>
2013-08-16 17:31:22 +0200

    contentinum backend administration
    Change-Id: I897a3e832608705b152cf07cfe4231509b1de471

---
+++ module/Mcwork/config/controllers.config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+		'invokables' => array(
+					'Mcwork\Controller\Index' => 'Mcwork\Controller\IndexController',
+					'Mcwork\Controller\Accounts' => 'Mcwork\Controller\AccountsController',
+		),
+);
