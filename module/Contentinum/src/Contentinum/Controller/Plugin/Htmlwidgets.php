4fa81fa3458ace43eca35001e6ce5f4342eb9274
mikel1961 <michael.jochum@jochum-mediaservices.de>
2013-10-01 10:48:52 +0200

    Controller plugin to select html widgets datas from a xml file
    Change-Id: I66b782d2cc0e9361f38e30f53c0ba530c074612c

---
+++ module/Contentinum/src/Contentinum/Controller/Plugin/Htmlwidgets.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * contentinum - accessibility websites
+ *
+ * LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category contentinum
+ * @package Controller Plugin
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ * @copyright Copyright (c) 2009-2013 jochum-mediaservices, Katja Jochum (http://www.jochum-mediaservices.de)
+ * @license http://www.opensource.org/licenses/bsd-license
+ * @since contentinum version 5.0
+ * @link      https://github.com/Mikel1961/contentinum-components
+ * @version   1.0.0
+ */
+namespace Contentinum\Controller\Plugin;
+
+use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+
+/**
+ * Access review
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ */
+class Htmlwidgets extends AbstractPlugin
+{
+
+	public function __invoke($htmlwidgets,$pagelist, $page)
+	{
+		if ( $pagelist->$page->htmlwidgets ){
+			$key = $pagelist->$page->htmlwidgets;
+			return $htmlwidgets->default->$key;
+		}
+		if ( $pagelist->_default->htmlwidgets ){
+			$key = $pagelist->_default->htmlwidgets;
+			return $htmlwidgets->default->$key;
+		}
+		return null;
+
+	}
+}
