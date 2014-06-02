e2743dcdd1c270318006cfeef21cbc50e20cdd2e
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2014-06-02 14:58:08 +0200

    Add a customer configuration data file

---
+++ data/locale/etc/custom.config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'default' => array(
+        'Medias' => array(
+            'alternate-sizes' => array(
+                'thumbnail' => '200',
+                'mobile' => '360',
+                '1024' => '800',
+                '1441' => '1200',
+                '1921' => ''
+            )
+        )
+    )
+);
