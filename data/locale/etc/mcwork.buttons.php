16500401ad2de807e044d2312484603627ceef84
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2014-09-02 14:02:40 +0200

    Customer form buttons configuration

---
+++ data/locale/etc/mcwork.buttons.php
@@ -0,0 +1,46 @@
+<?php
+return array(
+    'tag' => 'button',
+    'attribute' => array(
+        'class' => 'button-group right'
+    ),
+    'standards' => array(
+        'save' => array(
+            'label' => 'Save',
+            'href' => false,
+            'attribs' => array(
+                'title' => 'Save your form entries',
+                'class' => 'button form-button-save',
+                'role' => 'button'
+            )
+        ),
+        'saveplus' => array(
+            'label' => 'Save+',
+            'href' => false,
+            'attribs' => array(
+                'title' => 'Save your form entries and clear form',
+                'class' => 'button form-button-saveplus',
+                'role' => 'button'
+            )
+        ),        
+        'apply' => array(
+            'label' => 'Apply',
+            'href' => false,
+            'attribs' => array(
+                'title' => 'Save your form entries',
+                'class' => 'button form-button-apply',
+                'role' => 'button'
+            )
+        ),        
+        'cancel' => array(
+            'label' => 'Cancel',
+            'href' => false,
+            'attribs' => array(
+                'title' => 'clear allcache items',
+                'class' => 'button form-button-cancel',
+                'role' => 'button'
+            )
+        )
+    )
+    
+);
